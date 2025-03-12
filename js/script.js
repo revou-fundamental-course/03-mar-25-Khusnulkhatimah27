facedfa7280e15b5671502494a9a89499adfa7bd
KHUSNUL KHATIMAH <20102192@ittelkom-pwt.ac.id>
2025-03-12 21:01:24 +0800

    finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// INI FILE JAVASCRIPT
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('.nav-link');
+  
+    links.forEach(link => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: "smooth" });
+      });
+    });
+  });
+  
